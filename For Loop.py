ecb05614b67c38477e21d778b2ed60e891a2e999
Aashankk <103808212+Aashankk@users.noreply.github.com>
2022-04-16 01:24:02 -0400

    Add files via upload

---
+++ For Loop.py
@@ -0,0 +1,10 @@
+for index in range(5):
+    if index == 0 or index == 3:
+        print("match")
+    elif index == 2 or index == 1:
+        print("second match")
+    else:
+        print("not a match")
+
+
+
